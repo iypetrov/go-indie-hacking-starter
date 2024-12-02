877b34213258e9b3f3a8803b59613d6805180ad4
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-12-02 14:44:06 +0200

    chore: split handlers to thier own file

---
+++ handler.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/form"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"github.com/iypetrov/go-indie-hacking-starter/database"
+	"github.com/iypetrov/go-indie-hacking-starter/templates/components"
+	"github.com/iypetrov/go-indie-hacking-starter/templates/views"
+	"github.com/lib/pq"
+)
+
+type Handler struct {
+	formDecoder   *form.Decoder
+	formValidator *validator.Validate
+	conn          *sql.DB
+	queries       *database.Queries
+}
+
+func (h *Handler) HomeView(w http.ResponseWriter, r *http.Request) {
+	Render(w, r, views.PublicHome())
+}
+
+func (h *Handler) AddMailToMailingList(ctx context.Context, logger Logger, w http.ResponseWriter, r *http.Request) error {
+	err := r.ParseForm()
+	if err != nil {
+		AddToast(w, ErrorInternalServerError(ErrParsingFrom))
+		return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+	}
+	var input components.PublicMailingListFormInput
+	err = h.formDecoder.Decode(&input, r.Form)
+	if err != nil {
+		AddToast(w, ErrorInternalServerError(ErrDecodingForm))
+		return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+	}
+
+	err = h.formValidator.Struct(input)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+
+			AddToast(w, ErrorInternalServerError(ErrFailedtoValidateRequest))
+			return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			switch err.Field() {
+			case "Email":
+				if err.Tag() == "required" {
+					AddToast(w, WarningStatusBadRequest(WarnEmailIsRequred))
+					return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{Email: input.Email}))
+				} else if err.Tag() == "email" {
+					AddToast(w, WarningStatusBadRequest(WarnInvalidEmailFormat))
+					return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{Email: input.Email}))
+				}
+			}
+		}
+	}
+
+	output, err := h.queries.AddEmailToMailingList(
+		ctx,
+		database.AddEmailToMailingListParams{
+			ID:        uuid.New(),
+			Email:     input.Email,
+			CreatedAt: time.Now().UTC(),
+		},
+	)
+	if err != nil {
+		var pgErr *pq.Error
+
+		ok := errors.As(err, &pgErr)
+		if ok {
+			if pgErr.Code == "23505" {
+				AddToast(w, WarningStatusBadRequest(WarnEmailAlreadyExists))
+				return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+			}
+		}
+		AddToast(w, ErrorInternalServerError(ErrFailedToAddEmailToMailingList))
+		return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+	}
+	logger.Info("email %s was added to the mailing list", output.Email)
+
+	AddToast(w, SuccessStatusCreated(SuccEmailAddedToMailingList))
+	return Render(w, r, components.PublicMailingListForm(components.PublicMailingListFormInput{}))
+}
--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -19,9 +20,9 @@ func Render(w http.ResponseWriter, r *http.Request, c templ.Component) error {
 	return nil
 }
 
-func MakeTemplHandler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+func MakeTemplHandler(ctx context.Context, logger Logger, f func(ctx context.Context, logger Logger, w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
+		if err := f(ctx, logger, w, r); err != nil {
 			var t Toast
 			if errors.As(err, &t) {
 				AddToast(w, t)
