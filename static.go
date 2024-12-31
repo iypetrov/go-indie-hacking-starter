8f270229715c6e0ac19ea07f92a8360712151dfa
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-12-31 15:34:22 +0200

    feat: adjust the static files to have /static prefix

--- static.go
+++ static.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
 )
 
 //go:embed static
@@ -12,15 +13,16 @@ var staticFS embed.FS
 func StaticFiles(logger Logger) http.Handler {
 	if Profile == "local" {
 		logger.Info("serving static files from local directory")
-		return http.StripPrefix("/", http.FileServer(http.Dir("static")))
+		return http.StripPrefix("/static", http.FileServerFS(os.DirFS("static")))
 	}
 
 	fs, err := fs.Sub(staticFS, "static")
 	if err != nil {
 		logger.Info("serving static files from local directory")
-		return http.StripPrefix("/", http.FileServer(http.Dir("static")))
+		return http.StripPrefix("/static", http.FileServerFS(os.DirFS("static")))
 	}
 
 	logger.Info("serving static files from embedded FS")
-	return http.FileServer(http.FS(fs))
+	// return http.FileServer(http.FS(fs))
+	return http.StripPrefix("/static", http.FileServer(http.FS(fs)))
 }
